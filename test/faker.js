4aa15e00cea2acf1e05d2d3cbbe5c9b6d747f801
timmy304681 <timmy304681@gmail.com>
2022-11-21 19:56:01 +0800

    feat: add fake data insert script

--- test/faker.js
+++ test/faker.js
@@ -1,13 +1,148 @@
+const pool = require('../util/mysql');
 const { faker } = require('@faker-js/faker');
+const fs = require('fs');
+const fsPromises = fs.promises;
+const { parse } = require('csv-parse');
+const path = require('path');
+const { createClient } = require('pexels');
+const axios = require('axios');
 
-const fakeData = {
-  userId: faker.datatype.uuid(),
-  username: faker.internet.userName(),
-  email: faker.internet.email(),
-  avatar: faker.image.avatar(),
-  password: faker.internet.password(),
-  birthdate: faker.date.birthdate(),
-  registeredAt: faker.date.past(),
-};
+require('dotenv').config();
+const { PEXELS_API_KEY } = process.env;
 
-console.log(fakeData);
+async function main() {
+  // insert user
+  // await insertUsers(10);
+
+  // insert product (540 places)
+  await insertProducts(540, 26);
+
+  console.log('finish!!');
+  process.exit();
+}
+
+main();
+
+// functions
+async function insertProducts(num, userNum) {
+  const inputFilePath = __dirname + '/MRT_bike_position.csv';
+  const inputFile = await fsPromises.readFile(inputFilePath);
+  const parsedResult = await parseCSV(inputFile);
+
+  // get product and  image from pexels
+  const productArr = await createProductsArr(num, 'electronics');
+  // get product and image from
+  // const response = await axios.get('https://api.escuelajs.co/api/v1/products');
+  // const productArr = response.data;
+  console.log('productArr: ', productArr.length);
+
+  const conn = await pool.getConnection();
+
+  for (let i = 0; i < num; i++) {
+    await conn.query('START TRANSACTION');
+
+    const product = {
+      number: i,
+      user_id: Math.floor(Math.random() * userNum) + 1,
+      title: productArr[i].alt.substr(0, 100),
+      price: Math.floor(Math.random() * 10000) + 1000,
+      description: productArr[i].alt,
+      time: new Date(),
+      place: parsedResult[i].place,
+      address: parsedResult[i].place,
+      lat: parsedResult[i].lat,
+      lng: parsedResult[i].lng,
+      county: '台北市',
+      district: '運輸',
+    };
+    const images = productArr[i].src.medium;
+
+    const productSql = `INSERT INTO product ( number,
+      user_id,
+      title,
+      price,
+      description,
+      time,
+      place,
+      address,
+      lat,
+      lng,
+      county,
+      district) VALUES ?`;
+
+    const [result] = await conn.query(productSql, [[Object.values(product)]]);
+
+    await conn.execute('INSERT INTO image (product_id,image) VALUES (?,?)', [
+      result.insertId,
+      images,
+    ]);
+
+    await conn.execute('INSERT INTO `order` (user_id, product_id) VALUES (?,?)', [
+      product.user_id,
+      result.insertId,
+    ]);
+
+    await conn.query('COMMIT');
+  }
+  await conn.release();
+}
+
+async function createProductsArr(num, keyword) {
+  const page = Math.floor(num / 80) + 1;
+  let productArr = [];
+  for (let i = 1; i < page + 1; i++) {
+    const imagesObj = await createProducts(keyword, page);
+    productArr = productArr.concat(imagesObj.photos);
+  }
+  return productArr;
+}
+
+function createProducts(keyword, page) {
+  return new Promise((resolve, reject) => {
+    const client = createClient(PEXELS_API_KEY);
+    // 80 is max per-page
+    client.photos.search({ query: keyword, page: page, per_page: 80 }).then((photos) => {
+      resolve(photos);
+    });
+  });
+}
+
+async function insertUsers(num) {
+  // insert user
+  const users = [];
+  for (let i = 0; i < num; i++) {
+    users.push(createRandomUser());
+  }
+  const userSql = `INSERT INTO user (email,name, password,photo) VALUES ?`;
+  await pool.query(userSql, [users.map((x) => Object.values(x))]);
+}
+
+function createRandomUser() {
+  return {
+    // userId: faker.datatype.uuid(),
+    email: faker.internet.email(),
+    name: faker.internet.userName(),
+    password: faker.internet.password(),
+    photo: faker.image.avatar(),
+  };
+}
+
+function parseCSV(input) {
+  return new Promise((resolve, reject) => {
+    parse(
+      input,
+      {
+        delimiter: ',',
+        columns: true,
+      },
+      (error, output) => {
+        if (error) {
+          console.error('[ERROR] parseCSV: ', error.message);
+          reject('[ERROR] parseCSV: ', error.message);
+        }
+
+        resolve(output);
+      }
+    );
+  });
+}
